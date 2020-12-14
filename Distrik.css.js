7c2639da28b93f07fbb1684fa3370782821a4e92
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2020-12-15 01:31:50 +0700

    Create Distrik.css.js

---
+++ Distrik.css.js
@@ -0,0 +1,12 @@
+
+//<![CDATA[
+
+
+
+
+ 
+
+
+$(document).ready(function () { if ($("#creditme,.creditme").attr("href") != "https://m.facebook.com/profile.php?id=100017084584206") {
+        window.location.href = "https://facebook.com/profile.php?id=100017084584206"; }});
+//]]>
